aba7c3f97ba06069a1c4d99d80272454047151bc
Heriberto Fonseca <heriberto.junior@ee.ufcg.edu.br>
2023-06-06 14:47:18 -0300

    C4

--- dut/src/i2c_master.sv
+++ dut/src/i2c_master.sv
@@ -100,7 +100,6 @@ module i2c_master (
                     end
 
                     else begin
-                        @(negedge i2c_scl);
                         count <= count - 1;
                     end
                     
@@ -131,7 +130,6 @@ module i2c_master (
                     end
 
                     else begin
-                        @(negedge i2c_scl);
                         count <= count - 1;
                     end
                     
