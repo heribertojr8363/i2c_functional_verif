3b2c26c2ffadacde76d67428cadee64794ceaafc
Heriberto Fonseca <heriberto.fonseca@embedded.ufcg.edu.br>
2023-06-06 13:27:51 -0300

    Delete files

--- dut/tb/shm.tcl
+++
@@ -1,4 +0,0 @@
-database -open waves -shm
-probe -create tb -depth all -all -memories -shm -database waves
-run 18 ms -absolute
-exit
--- dut/tb/xcelium.d/run.lnx8664.20.09.d/.timestamp.ts
+++
@@ -1,2 +0,0 @@
-1685990873 /home/heriberto.junior/Desktop/i2c/i2c_functional_verif/dut/tb/i2c_tb.sv
-1685990444 /home/heriberto.junior/Desktop/i2c/i2c_functional_verif/dut/src/i2c_master.sv
