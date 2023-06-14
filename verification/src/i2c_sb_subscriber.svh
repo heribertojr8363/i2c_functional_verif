fffa86206625f120a56ca962981ed32ff820cb6d
Heriberto Fonseca <heriberto.fonseca@embedded.ufcg.edu.br>
2023-06-14 19:44:20 -0300

    Final version won with coverage

--- verification/src/i2c_sb_subscriber.svh
+++ verification/src/i2c_sb_subscriber.svh
@@ -11,6 +11,8 @@ class i2c_sb_subscriber extends uvm_subscriber#(i2c_sequence_item);
       i2c_scoreboard i2c_sb;
  
       $cast( i2c_sb, m_parent );
-      i2c_sb.check_i2c(t);
+      i2c_sb.check_i2c_start(t);
+      i2c_sb.check_i2c_rw(t);
+      i2c_sb.check_i2c_stop(t);
    endfunction: write
 endclass: i2c_sb_subscriber
